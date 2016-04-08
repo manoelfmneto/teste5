c3f903f83b321a3884ef6693b4336d5c2cf6c475
manoe <manoe@DESKTOP-0EBQNFC>
2016-04-07 21:38:03 -0300

    commit de uma classe

---
+++ src/Classe.py
@@ -0,0 +1,17 @@
+'''
+Created on 7 de abr de 2016
+
+@author: manoe
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        

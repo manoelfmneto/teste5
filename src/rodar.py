577820fce82d1baaadbdb5a4cec2df975ace0876
manoe <manoe@DESKTOP-0EBQNFC>
2016-04-08 11:08:26 -0300

    primeira modificacao

--- src/rodar.py
+++ src/rodar.py
@@ -6,6 +6,7 @@ Created on 6 de abr de 2016
 from Plotter import Plotter
 
 if __name__ == '__main__':
+    
     p = Plotter([2,2], [-3,1])
-    print(p.valor(2))
+    print(p.valor(2)) #calcula y para x = 2
     
